d6bcfae6d115b2c26942845b4721c22f958c6eb6
Sercan Inaler <sercan@gmail.com>
2019-11-11 10:46:53 +0100

    add new files

--- App.js
+++ App.js
@@ -1,19 +1,4 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React  from 'react'
+import App from './src/App'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Test</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './index.css';
+import './App.css';
+
+export const treesURL = 'https://s3.eu-central-1.amazonaws.com/ecosia-frontend-developer/trees.json';
+
+const App = () => {
+  const [ trees, setTrees ] = useState([]);
+
+  const getTrees = async () => {
+    const response = await axios.get(treesURL)
+    const trees = response.data.trees
+    setTrees(trees);
+  }
+
+  /*
+  useEffect(() => {
+    getTrees();
+  }, []);
+  */
+
+  const toggleTree = id => {
+    const newTrees = [...trees];
+    newTrees[id].showImage = !newTrees[id].showImage;
+    setTrees(newTrees);
+  }
+
+  return (
+    <div className="App">
+      <div className="App-tree-items-holder">
+        {trees && trees.length !== 0 ? <div className="App-tree-items">
+          {trees.map((tree, index) => (
+            <div className="App-tree-item" key={tree.name} data-testid="row">
+              <h1 className="App-tree-item-title">{tree.name}</h1>
+              <div className="App-tree-item-subtitle">{tree.species_name}</div>
+
+              {tree.showImage && <img src={tree.image} alt={tree.name} className="App-tree-item-image" />}
+
+              <button
+                name={index}
+                type="submit"
+                className="App-tree-item-button"
+                onClick={() => toggleTree(index)}
+              >
+                {tree.showImage ? 'Hide' : 'Show' } Image
+              </button>
+            </div>
+          ))}
+        </div> : <div>
+          <button onClick={getTrees} className="App-tree-item-button">
+            Load awesome trees
+          </button>
+        </div>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
