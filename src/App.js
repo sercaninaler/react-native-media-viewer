de570da75f35b82df766c54010835215a0188257
Sercan Inaler <sercan@gmail.com>
2019-11-14 13:51:57 +0100

    add search

--- src/App.js
+++ src/App.js
@@ -4,35 +4,30 @@ import './index.css'
 import './App.css'
 import { PIXABAY_API_URL, PIXABAY_API_KEY, FREESOUND_API_URL, FREESOUND_API_KEY } from '../config'
 
-let query = 'dog'
-
-export const pixabayApi = `${PIXABAY_API_URL}?key=${PIXABAY_API_KEY}&q=${query}&image_type=photo`
-export const freesoundApi = `${FREESOUND_API_URL}?query=${query}&token=${FREESOUND_API_KEY}
-&normalized=true&fields=previews&sort=downloads_desc&filter=duration:[1 TO 5]`
+export const pixabayApi = query => `${PIXABAY_API_URL}?key=${PIXABAY_API_KEY}&q=${query}&image_type=photo`
+export const freesoundApi = query =>`${FREESOUND_API_URL}?query=${query}&token=${FREESOUND_API_KEY}
+&normalized=true&fields=previews,description&sort=downloads_desc&filter=duration:[1 TO 5]`
 
 const App = () => {
   const [ pictures, setPictures ] = useState([])
   const [ sounds, setSounds ] = useState([])
+  const [ query, setQuery ] = useState('cats')
   let sound = null
 
-  const getPictures = async () => {
-    const response = await axios.get(pixabayApi)
+  const getPictures = async (pictureQuery) => {
+    const response = await axios.get(pixabayApi(pictureQuery))
     const pictures = response.data.hits
     //console.log(pictures)
     setPictures(pictures)
   }
 
-  const getSounds = async () => {
-    const response = await axios.get(freesoundApi)
+  const getSounds = async (soundQuery) => {
+    const response = await axios.get(freesoundApi(soundQuery))
     const sounds = response.data.results
     console.log(sounds)
     setSounds(sounds)
+    playAudio(sounds[0].previews['preview-lq-mp3'])
   }
-
-  useEffect(() => {
-    getPictures()
-    getSounds()
-  }, [])
 
   const togglePicture = id => {
     const newPictures = [...pictures]
@@ -48,18 +43,41 @@ const App = () => {
     sound.play()
   }
 
+  const onSubmit = event => {
+    event.preventDefault()
+    getPictures(query)
+  }
+
+  const onChange = event => {
+    console.log(event.target.value)
+    setQuery(event.target.value)
+  }
+//
   return (
     <div className="App">
       <div className="App-picture-items-holder">
-        {pictures && pictures.length !== 0 ? <div className="App-picture-items">
-          {pictures.map((picture, index) => (
-            <div className="App-picture-item" key={picture.id} data-testid="row">
+        <form onSubmit={onSubmit}>
+          <input
+            name="searchQuery"
+            type="text"
+            className="Search-input"
+            placeholder="Search for pictures"
+            autoComplete="off"
+            onChange={onChange}
+            value={query}
+          />
+          <button className="Search-button">
+            Search
+          </button>
+        </form>
 
+        {pictures && pictures.length !== 0 && <div className="App-picture-items">
+          {pictures.map((picture, index) => (
+            <div className="App-picture-item" key={picture.id} data-testid="row" onClick={() => { getSounds(query) }}>
               {!picture.showImage && <img
                 src={picture.webformatURL}
                 alt={picture.tags}
                 className="App-picture-item-image"
-                onClick={() => { playAudio(sounds[index].previews['preview-lq-mp3']) }}
               />}
 
               <h2 className="App-picture-item-title">{picture.tags}</h2>
@@ -68,7 +86,7 @@ const App = () => {
                 <button
                   name={index}
                   type="submit"
-                  className="App-picture-item-button"
+                  className="Search-button"
                   onClick={() => togglePicture(index)}
                 >
                   {picture.showImage ? 'Hide' : 'Show' } Image
@@ -76,10 +94,6 @@ const App = () => {
               </div>}
             </div>
           ))}
-        </div> : <div>
-          <button onClick={getPictures} className="App-picture-item-button">
-            Load awesome animal pictures
-          </button>
         </div>}
       </div>
     </div>
