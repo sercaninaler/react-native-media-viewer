ce182dab72a7cc3223918b4ae8f09b36e4d1508c
Sercan Inaler <sercan@gmail.com>
2020-01-07 10:24:34 +0100

    Updates

--- precache-manifest.12beafea81f766fe23094e49cb338580.js
+++ precache-manifest.12beafea81f766fe23094e49cb338580.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "2dacf6f059f19661d83f",
-    "url": "/react-native-media-viewer/static/js/app.9368768a.chunk.js"
+    "revision": "a76fbf49efcca07351e0",
+    "url": "/react-native-media-viewer/static/js/app.abd4153c.chunk.js"
   },
   {
     "revision": "b651ec6ee3f07fc88480",
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/react-native-media-viewer/./fonts/UbuntuMono-R.ttf"
   },
   {
-    "revision": "5e695e96a003a79f7f97060bf49409a9",
-    "url": "/react-native-media-viewer/expo-service-worker.js"
-  },
-  {
     "revision": "7a7bc7ead25db795e58b336f04d2624c",
     "url": "/react-native-media-viewer/favicon.ico"
   },
   {
     "revision": "d0c694b562b2208635f250762cd7fc79",
     "url": "/react-native-media-viewer/serve.json"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/react-native-media-viewer/expo-service-worker.js"
   },
   {
     "revision": "020fd499a2d2bc45c4c3b6021e472b43",
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "/react-native-media-viewer/manifest.json"
   },
   {
-    "revision": "e4bc9ff885527f3133a4b156581a9ea5",
+    "revision": "1ee14fd2f7216ea9f71ef37460230b24",
     "url": "/react-native-media-viewer/index.html"
   },
   {
@@ -44,12 +44,12 @@ self.__precacheManifest = [
     "url": "/react-native-media-viewer/static/js/runtime~app.f99f8593.js.gz"
   },
   {
-    "revision": "08a2a3b6303a3951458abeb411b0a513",
-    "url": "/react-native-media-viewer/static/js/app.9368768a.chunk.js.gz"
-  },
-  {
     "revision": "bd56db0844d50841ee6e0b69eb5e794d",
     "url": "/react-native-media-viewer/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "fc3bbbe2191f969cd3f93f835a34d605",
+    "url": "/react-native-media-viewer/static/js/app.abd4153c.chunk.js.gz"
   },
   {
     "revision": "e84b25642ab93b0a9a89178014f50762",
